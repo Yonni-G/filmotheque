d82126a554c1cfa07cd8695deb1bf31957b6eb7a
Yonni-G <yonni4@gmail.com>
2024-12-06 13:49:43 +0100

    "eni"

--- src/main/java/fr/eni/filmotheque/filmotheque/bo/Membre.java
+++ src/main/java/fr/eni/filmotheque/filmotheque/bo/Membre.java
@@ -1,4 +1,47 @@
 package fr.eni.filmotheque.filmotheque.bo;
 
-public class Membre {
+public class Membre extends Personne{
+
+    private String pseudo;
+    private String motDePasse;
+    private boolean admin;
+
+    public Membre(String nom, String prenom, String pseudo, String motDePasse, boolean admin) {
+        super(nom, prenom);
+        this.pseudo = pseudo;
+        this.motDePasse = motDePasse;
+        this.admin = admin;
+    }
+
+    @Override
+    public String toString() {
+        return "Membre{" +
+                "pseudo='" + pseudo + '\'' +
+                ", admin=" + admin +
+                '}';
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
 }
--- src/main/java/fr/eni/filmotheque/filmotheque/bo/Personne.java
+++ src/main/java/fr/eni/filmotheque/filmotheque/bo/Personne.java
@@ -1,4 +1,46 @@
 package fr.eni.filmotheque.filmotheque.bo;
 
-public class Personne {
+public abstract class Personne {
+    private int id;
+    private String nom;
+    private String prenom;
+
+
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    @Override
+    public String toString() {
+        return "Personne{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
 }
